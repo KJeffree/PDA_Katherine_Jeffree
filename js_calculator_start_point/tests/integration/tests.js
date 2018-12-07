fa27a9b61a619c2c41cc9835192b1cb5802e499b
Katie Jeffree <k.jeffree@gmail.com>
2018-12-07 16:32:32 +0000

    updated evidence document

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -89,4 +89,14 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('NaN')
   })
 
+  it('does not display NAN (Not a Number) when dividing by any other number', function(){
+    element(by.css('#number7')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('14')
+  })
+
 });
